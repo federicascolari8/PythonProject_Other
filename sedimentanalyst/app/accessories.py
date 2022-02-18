e2299941b975b466d1e8901ec980dcf9280e8e9b
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2022-02-18 20:53:08 +0100

    updated returns from parse_contents method

--- sedimentanalyst/app/accessories.py
+++ sedimentanalyst/app/accessories.py
@@ -118,75 +118,65 @@ class Accessories:
         content_type, content_string = contents.split(',')
 
         decoded = base64.b64decode(content_string)
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded), engine="openpyxl", header=None)
+
+        # clean the dataset by catching only inputs indicated with the indexes
+        dff = df.copy()
+        columns_to_get = [input_dict_app["gs_clm"], input_dict_app["cw_clm"]]
+        dff_gs = dff.iloc[input_dict_app["header"]: input_dict_app["header"] + input_dict_app["n_rows"],
+                 columns_to_get]
+        dff_gs.reset_index(inplace=True, drop=True)
+        dff_gs = dff_gs.astype(float)
+
+        # Get metadata from the dataframe
+        # get sample name
         try:
-            if 'csv' in filename:
-                # Assume that the user uploaded a CSV file
-                df = pd.read_csv(
-                    io.StringIO(decoded.decode('utf-8')))
-            elif 'xls' in filename:
-                # Assume that the user uploaded an excel file
-                df = pd.read_excel(io.BytesIO(decoded), engine="openpyxl", header=None)
+            samplename = dff.iat[input_dict_app["index_sample_name"][0], input_dict_app["index_sample_name"][1]]
+        except:
+            samplename = None
+            pass
 
-            # clean the dataset by catching only inputs indicated with the indexes
-            dff = df.copy()
-            columns_to_get = [input_dict_app["gs_clm"], input_dict_app["cw_clm"]]
-            dff_gs = dff.iloc[input_dict_app["header"]: input_dict_app["header"] + input_dict_app["n_rows"],
-                     columns_to_get]
-            dff_gs.reset_index(inplace=True, drop=True)
-            dff_gs = dff_gs.astype(float)
+        # get sample date
+        try:
+            sampledate = dff.iat[input_dict_app["index_sample_date"][0], input_dict_app["index_sample_date"][1]]
+        except:
+            sampledate = None
+            pass
 
-            # Get metadata from the dataframe
-            # get sample name
-            try:
-                samplename = dff.iat[input_dict_app["index_sample_name"][0], input_dict_app["index_sample_name"][1]]
-            except:
-                samplename = None
-                pass
+        # get sample coordinates
+        try:
+            lat = dff.iat[input_dict_app["index_lat"][0], input_dict_app["index_lat"][1]]
+            long = dff.iat[input_dict_app["index_long"][0], input_dict_app["index_long"][1]]
+        except:
+            lat, long = None, None
+            pass
 
-            # get sample date
-            try:
-                sampledate = dff.iat[input_dict_app["index_sample_date"][0], input_dict_app["index_sample_date"][1]]
-            except:
-                sampledate = None
-                pass
+        # get porosity
+        try:
+            porosity = dff.iat[input_dict_app["porosity"][0], input_dict_app["porosity"][1]]
+        except:
+            porosity = None
+            pass
 
-            # get sample coordinates
-            try:
-                lat = dff.iat[input_dict_app["index_lat"][0], input_dict_app["index_lat"][1]]
-                long = dff.iat[input_dict_app["index_long"][0], input_dict_app["index_long"][1]]
-            except:
-                lat, long = None, None
-                pass
+        # get sf_porosity
+        try:
+            sf_porosity = dff.iat[input_dict_app["SF_porosity"][0], input_dict_app["SF_porosity"][1]]
+        except:
+            sf_porosity = 6.1  # default for rounded sediments
+            pass
 
-            # get porosity
-            try:
-                porosity = dff.iat[input_dict_app["porosity"][0], input_dict_app["porosity"][1]]
-            except Exception as e:
-                porosity = None
-                print(e)
-                pass
+        metadata = [samplename, sampledate, (lat, long), porosity, sf_porosity]
 
-            # get sf_porosity
-            try:
-                sf_porosity = dff.iat[input_dict_app["SF_porosity"][0], input_dict_app["SF_porosity"][1]]
-            except Exception as e:
-                sf_porosity = 6.1  # default for rounded sediments
-                print(e)
-                pass
+        # Rename and standardize the Grain Size dataframe
+        dff_gs.rename(columns={dff_gs.columns[0]: "Grain Sizes [mm]", dff_gs.columns[1]: "Fraction Mass [g]"},
+                      inplace=True)
 
-            metadata = [samplename, sampledate, (lat, long), porosity, sf_porosity]
+        analyzer = StatisticalAnalyzer(sieving_df=dff_gs, metadata=metadata)
 
-            # Rename and standardize the Grain Size dataframe
-            dff_gs.rename(columns={dff_gs.columns[0]: "Grain Sizes [mm]", dff_gs.columns[1]: "Fraction Mass [g]"},
-                          inplace=True)
-
-            analyzer = StatisticalAnalyzer(sieving_df=dff_gs, metadata=metadata)
-
-        except Exception as e:
-            print(e)
-            return html.Div([filename,
-                             ': There was an error processing the file. '
-                             'Ensure that your file does not contain too many columns (< 15).'
-                             ])
-
-        return analyzer, html.Div([filename, ': File successfully read'])
+        return analyzer
